1597c899c76dcec1907e59f6fefc6781bfdd6591
Ivan Sosniuk <i.sosniuk@ukma.edu.ua>
2026-01-14 15:08:27 +0200

    Change bash shebang from absolute path to relative

--- python_practice_1.py
+++ python_practice_1.py
@@ -1,4 +1,4 @@
-#!/mnt/ntfs/media_shared/ukma/python/python_projects/python-for-big-data-and-data-science/.venv/bin/python3
+#!.venv/bin/python3
 
 if __name__ == '__main__':
     print("Ivan Sosniuk, IPZ-2026")
